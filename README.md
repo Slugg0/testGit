fa97f36d6d4b710043c47a7e514fec0f299b4580
Sluggo <slugg0@users.noreply.github.com>
2015-02-08 01:54:17 +0100

    Added content to testfiles

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the readmefile.
+=======================
+
+Project description here.
---
+++ hello.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang='<?=$lang?>'>
+<head>
+<meta charset='utf-8'/>
+<title>Hello World</title>
+</head>
+<body>
+	<h1>Hello!</h1>
+	<?="This is a test- echo"?>
+</body>
+</html>
